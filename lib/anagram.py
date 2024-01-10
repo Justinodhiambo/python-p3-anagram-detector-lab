2648723ecb15122a6bb77a8752ab58efe0fe8806
Justin Odhiambo <justinsibudiodhiambo@gmail.com>
2024-01-10 07:52:10 -0500

    Completed Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, candidates):
+        return [candidate for candidate in candidates if self.is_anagram(candidate)]
+
+    def is_anagram(self, candidate):
+        candidate = candidate.lower()
+        return sorted(candidate) == sorted(self.word) and candidate != self.word
